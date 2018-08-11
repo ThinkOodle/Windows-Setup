0d5d6deb4d0ad5ee9cb32b02c88533efd0ab1f2e
ryanrhughes <ryan@oodle.io>
2018-08-11 01:39:59 -0400

    Update wsl-setup.sh

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -3,6 +3,7 @@ sudo apt install python3
 sudo apt install build-essential
 
 # NVM + Yarn
+export NVM_DIR="" #Hackish way to make NVM not get confused
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | sh
 nvm install --lts
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
